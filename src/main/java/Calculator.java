c118208b52161e55da1d90188bce8fba14e9a24f
Thotsana <thotsana.mabotsa@umuzi.org>
2020-01-22 20:59:05 +0200

    third commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,28 +1,47 @@
 import java.util.regex.*;
 
+/**
+ * A simple method that takes a string as input and returns the sum of the numbers entered in the string.
+ * @author Joshua Thotsana Mabotsa.
+ */
+
 public class Calculator {
 
+    // initialized instance variable
     static String numberPatterns = "-\\d+|\\d+";
 
+    /**
+     * Main method signature
+     * @param args String argument.
+     * @throws Exception invokes a static method that throws exception.
+     */
     public static void main(String[] args) throws Exception {
 
-        System.out.println(add("//***\n1***2***3"));
+        System.out.println(add(""));
 
     }
 
+    /**
+     * A method that takes a string as a parameter and returns the sum of the digits in the string.
+     * @param string            Takes a string as a parameter.
+     * @return                  Returns the total sum of the numbers in the string.
+     * @throws Exception        Throws exception if the string is not in the required format.
+     */
     static int add(String string) throws Exception {
 
-        if (string.length() >=2 && string.substring(0, 2).equals("//")) {
+        if (string.length() >=2 && string.substring(0, 2).equals("//")) {   // check if a string has delimiters.
 
             Pattern delimiter = Pattern.compile("\\[.*?\\]");
             Matcher matcherDelimiter = delimiter.matcher(string);
 
-            while (matcherDelimiter.find()) {
+            while (matcherDelimiter.find()) {             // find all delimiters and replace them with commas.
+
                 int closeBraket = matcherDelimiter.group().indexOf("]");
                 string = string.replace(matcherDelimiter.group().substring(1,closeBraket),",");
+
             }
 
-            string =  string.replace(string.substring(2,string.indexOf("\n")),",");
+            string =  string.replace(string.substring(2,string.indexOf("\n")),","); // replace //[delimiter]...[delimiter]\n..., with //,\n
             string = string.replace("\n","");
         }
         else {
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,17 +1,22 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
-    String pattern = "-\\d+|\\d+";
-
     @Test
     void add() throws Exception {
 
-        String input = "";
+        String input1 = "1,2,3,4";
 
-        assertEquals(0,Calculator.add(input));
+        assertEquals(10,Calculator.add(input1));
+
+        String input2 = "//;\n1000;1;2;";
+
+        Assertions.assertThrows(Exception.class, () -> {
+            Calculator.add(input2);
+        });
 
     }
 }
